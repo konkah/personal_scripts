4d7afa33e8609b701ad9284cad4fdbb26b2699f8
Karlos Helton Braga <khbraga@huia.haus>
2021-12-01 00:28:44 -0300

    Add personal script for bashrc

---
+++ personal_terminal.sh
@@ -0,0 +1,11 @@
+echo "echo ''" >> ~/.bashrc
+echo "echo '  #~~~~~~~~~~~~~~~~~~~#'" >> ~/.bashrc
+echo "echo '  {                   }'" >> ~/.bashrc
+echo "echo '  {  OHANA, GAMBARE!  }'" >> ~/.bashrc
+echo "echo '  {                   }'" >> ~/.bashrc
+echo "echo '  #~~~~~~~~~~~~~~~~~~~#'" >> ~/.bashrc
+echo "echo ''" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "export PS1='\\n\\[\\e[1m\\]\\u@\\h\\[\\e[0m\\]:\\[\\033[01;31m\\]\\w\\[\\033[0;32m\\]\$(__git_ps1 \"\\n(%s)\")\\n$ \\[\\033[00m\\]'" >> ~/.bashrc
+echo "export PS1_marker=16" >> ~/.bashrc
+
